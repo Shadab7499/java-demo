30d1967812aabbf21b35449f2f7c37eb2333a3de
Shadab Chaudhary <sahilkhan749982@gmail.com>
2024-05-20 22:36:34 +0530

    Initial commit

---
+++ src/EmployeeManagement.java
@@ -0,0 +1,10 @@
+public class EmployeeManagement {
+    public static void main(String[] args) {
+       Employee emp1 = new Employee(20, "Shadab");
+        Employee emp2 = new Employee(26, "Sahil");
+        System.out.println(emp1.getId() + " " + emp1.getName());
+        System.out.println(emp2.getId() + " " + emp2.getName());
+
+
+    }
+}
